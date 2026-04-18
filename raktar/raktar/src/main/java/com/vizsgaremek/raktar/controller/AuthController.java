3eef07943069cd74ee976e1627d358e4eae3706e
tothgergo123 <21d_tothg@nyirszikszi.hu>
2026-04-18 16:50:44 +0200

    Enable CORS in AuthController
    Added @CrossOrigin annotation to allow cross-origin requests.

--- raktar/raktar/src/main/java/com/vizsgaremek/raktar/controller/AuthController.java
+++ raktar/raktar/src/main/java/com/vizsgaremek/raktar/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
