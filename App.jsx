e5d52f7d7e172654872d614ef904597c100bafce
Peto2007 <91938696+Peto2007@users.noreply.github.com>
2026-04-18 16:18:55 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
 
+
 import LoginPage from "./pages/LoginPage";
 import Index from "./pages/Index";
 import KeszletRoute from "./pages/KeszletRoute";
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8081/api';
 
 function App() {
   const [user, setUser] = useState(null);
