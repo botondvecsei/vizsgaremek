9d929542c13d4ea2ba177fca08d54624f24cfcfc
tothgergo123 <21d_tothg@nyirszikszi.hu>
2026-04-18 16:52:57 +0200

    Enable CORS for TermekController
    Added CrossOrigin annotation to allow requests from any origin.

--- raktar/raktar/src/main/java/com/vizsgaremek/raktar/controller/TermekController.java
+++ raktar/raktar/src/main/java/com/vizsgaremek/raktar/controller/TermekController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/termek")
 @RequiredArgsConstructor
 public class TermekController {
