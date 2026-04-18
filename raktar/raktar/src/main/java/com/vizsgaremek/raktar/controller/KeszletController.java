9ed8f48ef08549e3552e53a2869f27dcef188adf
tothgergo123 <21d_tothg@nyirszikszi.hu>
2026-04-18 16:52:05 +0200

    Add CrossOrigin annotation to KeszletController

--- raktar/raktar/src/main/java/com/vizsgaremek/raktar/controller/KeszletController.java
+++ raktar/raktar/src/main/java/com/vizsgaremek/raktar/controller/KeszletController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/keszlet")
 @RequiredArgsConstructor
 public class KeszletController {
